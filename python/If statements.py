d9b666d7c92d207142e37cde71e572697c0b7e40
AryaMath <arya@mathewj.com>
2014-11-02 10:18:42 -0800

    Week 3 making lists.
    learning how to make lists and tables.

---
+++ python/If statements.py
@@ -0,0 +1,10 @@
+print ("Give a number.")
+num1 = input ()
+print ("Give another number.")
+num2 = input ()
+if (num1 < num2):
+    print ("B")
+elif (num1 > num2):
+    print ("A")
+else 
+    print ("C")
